97a77e4b5d3cb4e385579001bb6f62d90bad612a
Worawalan C. <113490970+woraamy@users.noreply.github.com>
2025-03-15 21:44:43 +0700

    Add zod form for verification

--- frontend/src/app/flights/[id]/page.tsx
+++ frontend/src/app/flights/[id]/page.tsx
@@ -4,25 +4,24 @@ import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 import { format } from "date-fns";
 import { 
-  Plane, Clock, Wifi, Tv, UtensilsCrossed, Star, LoaderCircle, PlaneTakeoff, PlaneLanding, CalendarArrowUp, CalendarArrowDown, ChevronDown
+  Plane, Wifi, Tv, UtensilsCrossed, Star, LoaderCircle, PlaneTakeoff, PlaneLanding, CalendarArrowUp, CalendarArrowDown, ChevronDown
 } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 import { flightAPI } from "./action";
-import { Apple } from "lucide-react";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
-import { Switch } from "@/components/ui/switch";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
-import { FaChevronDown } from "react-icons/fa";
 
 interface Flight {
   id: number;
   flight_number: string;
-  departure_airport: { code: string; name: string };
-  arrival_airport: { code: string; name: string };
+  departure_airport: { code: string; name: string; city: string };
+  arrival_airport: { code: string; name: string; city: string};
   departure_time: string;
   arrival_time: string;
   base_price: number;
@@ -34,6 +33,21 @@ interface Flight {
   has_meals: boolean;
 }
 
+const passengerSchema = z.object({
+  firstName: z.string().min(2, "First name must be at least 2 characters"),
+  lastName: z.string().min(2, "Last name must be at least 2 characters"),
+  age: z.number().min(1, "Age must be at least 1").max(120, "Age must be less than 120"),
+  gender: z.enum(["male", "female", "prefer_not_to_say"]),
+  contactNumber: z
+    .string()
+    .regex(/^\+\d{7,15}$/, "Invalid phone number. Example: +123456789"),
+  email: z.string().email("Invalid email address"),
+  passportNumber: z.string().min(6, "Invalid passport number"),
+  nationality: z.string().min(3, "Nationality must be valid"),
+});
+
+type PassengerFormData = z.infer<typeof passengerSchema>;
+
 export default function FlightDetailsPage() {
   const { id: flightId } = useParams<{ id: string }>(); 
   const [flight, setFlight] = useState<Flight | null>(null);
@@ -43,6 +57,18 @@ export default function FlightDetailsPage() {
   const [mealExpanded, setMealExpanded] = useState(false);
   const [serviceExpanded, setServiceExpanded] = useState(false);
   const [baggageExpanded, setBaggageExpanded] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<PassengerFormData>({
+    resolver: zodResolver(passengerSchema),
+  });
+
+  const onSubmit = (data: PassengerFormData) => {
+    console.log("Valid Data Submitted:", data);
+  };
 
   useEffect(() => {
     if (!flightId) {
@@ -107,7 +133,7 @@ export default function FlightDetailsPage() {
       {/* Header */}
       <div className="flex flex-col space-y-4">
         <p className="text-3xl font-semibold text-center">Flight Details</p>
-        <h1 className="text-xl text-center">from {flight.departure_airport.name} to {flight.arrival_airport.name}</h1>
+        <h1 className="text-xl text-center">from {flight.departure_airport.city} to {flight.arrival_airport.city}</h1>
         <p className="text-sm flex items-center"><Star className="text-yellow-500 mr-1" /> Ratings {flight.rating} / 5</p>
         {/* Container for cards and button */}
         <img
@@ -180,83 +206,92 @@ export default function FlightDetailsPage() {
         <p className="text-xl font-semibold">Additional Services</p>
         {/* Services List */}
         {/* <ChevronDown className={`cursor-pointer transition-transform ${expanded ? "rotate-180" : ""}`} onClick={() => setExpanded(!expanded)} /> */}
-        <div className="flex justify-between">
-          <span>
-            <p className="font-bold">Seat Preference</p>
-            <p>Select your preferred seat</p>
-          </span>
-          <ChevronDown id="seat preference" className={`cursor-pointer transition-transform ${seatExpanded ? "rotate-180" : ""}`} onClick={() => setSeatExpanded(!seatExpanded)} />
-        </div>
-        <div className="flex justify-between">
-          <span>
-            <p className="font-bold">Meal Preference</p>
-            <p>Choose your Meal</p>
-          </span>
-          <ChevronDown id="meal preference" className={`cursor-pointer transition-transform ${mealExpanded ? "rotate-180" : ""}`} onClick={() => setMealExpanded(!mealExpanded)} />
-        </div>
-        <div className="flex justify-between">
-          <span>
-            <p className="font-bold">Extra Bagage</p>
-            <p>Add extra baggage</p>
-          </span>
-          <ChevronDown id="seat preference" className={`cursor-pointer transition-transform ${baggageExpanded ? "rotate-180" : ""}`} onClick={() => setBaggageExpanded(!baggageExpanded)} />
-        </div>
-        <div className="flex justify-between">
-          <span>
-            <p className="font-bold">Special Assistance</p>
-            <p>Request Assistance</p>
-          </span>
-          <ChevronDown id="seat preference" className={`cursor-pointer transition-transform ${serviceExpanded ? "rotate-180" : ""}`} onClick={() => setServiceExpanded(!serviceExpanded)} />
-        </div>
-      </div>
-      <Separator className="m-8" />
-      {/* Passenger Information */}
-      <div>
-        <p className="text-xl font-semibold pb-4">Passenger Information</p>
-        <div className="grid grid-cols-2 gap-4">
-          <span className="space-y-2">
-            <p className="font-semibold">First Name</p>
-            <Input className="text-gray-600 bg-[#E9F1ED] rounded-xl" type="firstName" placeholder="e.g. John" />
-          </span>
-          <span className="space-y-2">
-            <p className="font-semibold">Last Name</p>
-            <Input className="text-gray-600 bg-[#E9F1ED] rounded-xl" type="lastName" placeholder="e.g. Doe" />
-          </span>
-          <span className="space-y-2">
-            <p className="font-semibold">Age</p>
-            <Input className="text-gray-600 bg-[#E9F1ED] rounded-xl" type="age" placeholder="e.g. 29" />
-          </span>
-          <span className="space-y-2">
-            <p className="font-semibold">Gender</p>
-            <select className="text-gray-600 bg-[#E9F1ED] rounded-xl p-2 w-full">
-              <option value="female">Female</option>
-              <option value="male">Male</option>
-              <option value="prefer_not_to_say">Prefer Not To Say</option>
-            </select>
-          </span>
-          <span className="space-y-2">
-            <p className="font-semibold">Contact Number</p>
-            <Input className="text-gray-600 bg-[#E9F1ED] rounded-xl" type="contactNumber" placeholder="e.g. +12345678" />
-          </span>
-          <span className="space-y-2">
-            <p className="font-semibold">Email</p>
-            <Input className="text-gray-600 bg-[#E9F1ED] rounded-xl" type="email" placeholder="e.g. johndoe@gmail.com" />
-          </span>
-          <span className="space-y-2">
-            <p className="font-semibold">Passport Number</p>
-            <Input className="text-gray-600 bg-[#E9F1ED] rounded-xl" type="passportNumber" placeholder="e.g. X12345678" />
-          </span>
-          <span className="space-y-2">
-            <p className="font-semibold">Nationality</p>
-            <Input className="text-gray-600 bg-[#E9F1ED] rounded-xl" type="nationality" placeholder="e.g. American" />
-          </span>
+          <div className="flex justify-between">
+            <span>
+              <p className="font-bold">Seat Preference</p>
+              <p>Select your preferred seat</p>
+            </span>
+            <ChevronDown id="seat preference" className={`cursor-pointer transition-transform ${seatExpanded ? "rotate-180" : ""}`} onClick={() => setSeatExpanded(!seatExpanded)} />
+          </div>
+          <div className="flex justify-between">
+            <span>
+              <p className="font-bold">Meal Preference</p>
+              <p>Choose your Meal</p>
+            </span>
+            <ChevronDown id="meal preference" className={`cursor-pointer transition-transform ${mealExpanded ? "rotate-180" : ""}`} onClick={() => setMealExpanded(!mealExpanded)} />
+          </div>
+          <div className="flex justify-between">
+            <span>
+              <p className="font-bold">Extra Baggage</p>
+              <p>Add extra baggage</p>
+            </span>
+            <ChevronDown id="seat preference" className={`cursor-pointer transition-transform ${baggageExpanded ? "rotate-180" : ""}`} onClick={() => setBaggageExpanded(!baggageExpanded)} />
+          </div>
+          <div className="flex justify-between">
+            <span>
+              <p className="font-bold">Special Assistance</p>
+              <p>Request Assistance</p>
+            </span>
+            <ChevronDown id="seat preference" className={`cursor-pointer transition-transform ${serviceExpanded ? "rotate-180" : ""}`} onClick={() => setServiceExpanded(!serviceExpanded)} />
+          </div>
         </div>
         <Separator className="m-8" />
-      </div>
-      {/* Buttons */}
-      <div className="flex space-x-4">
-        <Button>Save Details</Button>
-        <Button>Cancle</Button>
+
+        {/* Passenger Information */}
+        <div>
+          <p className="text-xl font-semibold pb-4">Passenger Information</p>
+          <form onSubmit={handleSubmit(onSubmit)} className="grid grid-cols-2 gap-4">
+            <div>
+              <Label>First Name</Label>
+              <Input className="text-gray-600 bg-[#E9F1ED] rounded-xl p-2 w-full" {...register("firstName")} placeholder="e.g. John" />
+              {errors.firstName && <p className="text-red-500">{errors.firstName.message}</p>}
+            </div>
+            <div>
+              <Label>Last Name</Label>
+              <Input className="text-gray-600 bg-[#E9F1ED] rounded-xl p-2 w-full" {...register("lastName")} placeholder="e.g. Doe" />
+              {errors.lastName && <p className="text-red-500">{errors.lastName.message}</p>}
+            </div>
+            <div>
+              <Label>Age</Label>
+              <Input className="text-gray-600 bg-[#E9F1ED] rounded-xl p-2 w-full" type="number" {...register("age", { valueAsNumber: true })} placeholder="e.g. 29" />
+              {errors.age && <p className="text-red-500">{errors.age.message}</p>}
+            </div>
+            <div>
+              <Label>Gender</Label>
+              <select {...register("gender")} className="text-gray-600 bg-[#E9F1ED] rounded-xl p-2 w-full">
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="prefer_not_to_say">Prefer Not To Say</option>
+              </select>
+              {errors.gender && <p className="text-red-500">{errors.gender.message}</p>}
+            </div>
+            <div>
+              <Label>Contact Number</Label>
+              <Input className="text-gray-600 bg-[#E9F1ED] rounded-xl p-2 w-full" {...register("contactNumber")} placeholder="e.g. +123456789" />
+              {errors.contactNumber && <p className="text-red-500">{errors.contactNumber.message}</p>}
+            </div>
+            <div>
+              <Label>Email</Label>
+              <Input className="text-gray-600 bg-[#E9F1ED] rounded-xl p-2 w-full" {...register("email")} type="email" placeholder="e.g. johndoe@gmail.com" />
+              {errors.email && <p className="text-red-500">{errors.email.message}</p>}
+            </div>
+            <div>
+              <Label>Passport Number</Label>
+              <Input className="text-gray-600 bg-[#E9F1ED] rounded-xl p-2 w-full" {...register("passportNumber")} placeholder="e.g. X12345678" />
+              {errors.passportNumber && <p className="text-red-500">{errors.passportNumber.message}</p>}
+            </div>
+            <div>
+              <Label>Nationality</Label>
+              <Input className="text-gray-600 bg-[#E9F1ED] rounded-xl p-2 w-full" {...register("nationality")} placeholder="e.g. American" />
+              {errors.nationality && <p className="text-red-500">{errors.nationality.message}</p>}
+            </div>
+            <Separator className="col-span-2 my-4" />
+            <div className="col-span-2 flex space-x-4">
+              <Button type="submit">Save Details</Button>
+              <Button type="button">Cancel</Button>
+            </div>
+          </form>
+        <Separator className="m-8" />
       </div>
     </div>
     {/* Right */}
