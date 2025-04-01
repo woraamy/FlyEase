5ee67d2fd2a0a3022b0263bd67a8e8f903d9ed96
Chanun SUNTRAPIWAT <chanun.s@ku.th>
2025-04-01 21:11:26 +0700

    initial stripe

---
+++ frontend/src/components/EmbeddedCheckoutForm.tsx
@@ -0,0 +1,72 @@
+"use client";
+import { loadStripe } from "@stripe/stripe-js";
+import {
+  EmbeddedCheckoutProvider,
+  EmbeddedCheckout,
+} from "@stripe/react-stripe-js";
+import { useCallback, useRef, useState } from "react";
+
+interface EmbeddedCheckoutProps {
+  price: Number;
+  name: string;
+}
+
+export default function EmbeddedCheckoutForm({ price, name }: EmbeddedCheckoutProps) {
+  const stripePromise = loadStripe(
+    process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
+  );
+
+  const [showCheckout, setShowCheckout] = useState(false);
+  const modalRef = useRef<HTMLDialogElement>(null);
+
+  const fetchClientSecret = useCallback(() => {
+    return fetch("/api/embedded-checkout", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ price, name }),
+    })
+      .then((res) => res.json())
+      .then((data) => data.client_secret);
+  }, [price, name]);
+
+  const options = { fetchClientSecret };
+
+  const handleCheckoutClick = () => {
+    setShowCheckout(true);
+    modalRef.current?.showModal();
+  };
+
+  const handleCloseModal = () => {
+    setShowCheckout(false);
+    modalRef.current?.close();
+  };
+
+  return (
+    <div id="checkout" className="my-4">
+      <button className="btn" onClick={handleCheckoutClick}>
+        Stripe Checkout
+      </button>
+      <dialog ref={modalRef} className="modal">
+      <div className="bg-white p-6 rounded-lg shadow-2xl w-full max-w-2xl max-h-[90vh] overflow-y-auto">
+      <h3 className="font-bold text-lg">Embedded Checkout</h3>
+          <div className="py-4">
+            {showCheckout && (
+              <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
+                <EmbeddedCheckout />
+              </EmbeddedCheckoutProvider>
+            )}
+          </div>
+          <div className="modal-action">
+            <form method="dialog">
+              <button className="btn" onClick={handleCloseModal}>
+                Close
+              </button>
+            </form>
+          </div>
+        </div>
+      </dialog>
+    </div>
+  );
+}
